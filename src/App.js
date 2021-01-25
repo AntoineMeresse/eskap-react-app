63970165a4cf47c69f61c32962f653021943a674
Antoine Meresse <ant.meresse@gmail.com>
2021-01-25 14:11:38 +0100

    feat: EskapList init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import EskapList from './components/EskapList';
 
 function App() {
   return (
     <div className="App">
       <h1>Eskap-React-App</h1>
+      <EskapList/>
     </div>
   );
 }
---
+++ src/components/EskapList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EskapList() {
+    return (
+        <div className="eskapList">
+            <p>I'm an Escape List</p>
+        </div>
+    )
+}
+
+export default EskapList;
